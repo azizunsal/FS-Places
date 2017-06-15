f9a3790aab319ff702501e638dbc24e0d7a27479
Aziz Unsal <unsal.aziz@gmail.com>
2017-06-16 00:11:28 +0300

    Added webpack production config.

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,16 @@ let path = require('path');
 const src = path.join(__dirname, './src');
 const dist = path.join(__dirname, './docs');
 
-
 module.exports = {
 	context: src,
 	entry: [
-		// 'webpack-dev-server/client?http://localhost:2222',
-   		// 'webpack/hot/only-dev-server',
+		// The script refreshing the browser on none hot updates
+		'webpack-dev-server/client?http://localhost:2222',
+
+		 // For hot style updates
+   		'webpack/hot/only-dev-server',
+
+		// and the application
    		src + '/index.js'
 	],
 	output: {
@@ -15,14 +19,14 @@ module.exports = {
 		publicPath: '',
 		filename: 'main.min.js'
 	},
-  // devtool: 'eval-source-map',
-	// devServer: {
-	// 	contentBase: dist,
-	// 	inline: true,
-	// 	port: 2222,
-	// 	hot: true,
-    // quiet: true
-	// },
+	devtool: 'eval-source-map',
+	devServer: {
+		contentBase: dist,
+		inline: true,
+		port: 2222,
+		hot: true,
+		quiet: true
+	},
 	module: {
 		loaders: [
 			{
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,49 @@
+'use strict';
+var webpack = require('webpack');
+let path = require('path');
+const src = path.join(__dirname, './src');
+const dist = path.join(__dirname, './docs');
+
 module.exports = {
+  context: src,
+  devtool: 'eval-source-map',
+  entry: [src + '/index.js'],
   output: {
     path: path.join(__dirname, './docs'),
     filename: 'prod.bundle.js',
-    publicPath: publicPath
+    publicPath: dist
     // sourceMapFilename: '[name].map'
   },
 
+module: {
+		loaders: [
+			{
+				loader: 'babel',
+				test: /\.js$/,
+				exclude: /node_modules/,
+				query: {
+					presets: ['es2015', 'react']
+				}
+			},
+
+			{
+				test: /\.scss$/,
+				loaders: ['style', 'css', 'sass']
+			}
+		]
+	},  
+
   plugins: [
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
     }),
+
+    // new webpack.LoaderOptionsPlugin({
+    //   minimize: true,
+    //   debug: false
+    // }),
     new webpack.optimize.UglifyJsPlugin({
       beautify: false,
       mangle: {

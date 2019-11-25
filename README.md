8898e5e273de6261d9f352c3db8de355cb3f4b23
Aziz Unsal <unsal.aziz@gmail.com>
2019-11-25 11:00:31 +0300

    Added little modifications to the readme file, fixed the app port.

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-FourSquare Recommended Places with [React](https://facebook.github.io/react/) and [Redux](http://redux.js.org/)
-==
+# Intro
 
-## To Run the Application
-1. `npm install`
-2. `npm start`
-3. Open the browser and enter `http://localhost:2222/`
+FourSquare Recommended Places with [React](https://facebook.github.io/react/) and [Redux](http://redux.js.org/)
+
+## Install & Run the Application
+
+1. Go into the project folder.
+2. Run `npm install` to download dependencies.
+3. Run `npm start` to start development server.
+4. Open the browser and enter `http://localhost:8080/` (Actually this page opens after executing `npm start` command).
--- webpack.common.js
+++ webpack.common.js
@@ -1,44 +1,43 @@
-
 let path = require('path');
 const src = path.join(__dirname, './src');
 const dist = path.join(__dirname, './dist');
 
 
 module.exports = {
-	context: src,
-	entry: src + '/index.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
+    context: src,
+    entry: src + '/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
     },
-    
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['es2015', 'react']
-					}
-					
-				}
-			},	
-			{
-				test: /\.scss$/,
-				use: [
-					{
-						loader: "style-loader"
-					},
-					{
-						loader: "css-loader"
-					},
-					{
-						loader: "sass-loader"
-					},
-				]
-			}
-		]
-	}
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015', 'react']
+                    }
+
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "sass-loader"
+                    },
+                ]
+            }
+        ]
+    }
 };
